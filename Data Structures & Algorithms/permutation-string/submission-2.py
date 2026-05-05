698cfa0e60b55b065833a935cce407a06c68ca57
Roberto Ochoa Cuevas <129466817+Roberto0611@users.noreply.github.com>
2026-05-05 17:57:00 -0600

    Add: permutation-string - submission-2

---
+++ Data Structures & Algorithms/permutation-string/submission-2.py
@@ -0,0 +1,29 @@
+class Solution:
+    def checkInclusion(self, s1: str, s2: str) -> bool:
+        k = len(s1)
+        r = k - 1
+        OGCount = {}
+        count = {}
+        
+        # count s1
+        for c in s1:
+            OGCount[c] = OGCount.get(c,0) + 1
+
+        # count k elements in s2
+        for c in s2[:k]:
+            count[c] = count.get(c,0) + 1
+
+        if OGCount == count:
+            return True;
+    
+        # start window loop
+        while(r < len(s2) - 1):
+            r+=1
+            count[s2[r]] = count.get(s2[r],0) + 1
+            count[s2[r-k]] -= 1
+            if count[s2[r-k]] == 0:
+                del count[s2[r-k]]
+            if OGCount == count:
+                return True;
+
+        return False
