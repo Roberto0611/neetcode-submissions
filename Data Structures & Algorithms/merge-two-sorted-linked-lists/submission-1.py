eb44ee74d1464f051aed2cd3ca407f0c6f8d3c52
Roberto Ochoa Cuevas <129466817+Roberto0611@users.noreply.github.com>
2026-07-14 17:11:56 -0700

    Add: merge-two-sorted-linked-lists - submission-1

---
+++ Data Structures & Algorithms/merge-two-sorted-linked-lists/submission-1.py
@@ -0,0 +1,48 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+
+class Solution:
+    def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
+        curr1 = list1
+        curr2 = list2
+        head3 = None
+        # find the fist value
+        
+        if curr1 == None and curr2 == None:
+            return
+        elif curr1 == None and curr2 != None:
+            head3 = curr2
+            curr2 = curr2.next
+        elif curr2 == None and curr1 != None:
+            head3 = curr1
+            curr1 = curr1.next
+        elif curr1.val >= curr2.val:
+            head3 = curr2
+            curr2 = curr2.next
+        else:
+            head3 = curr1
+            curr1 = curr1.next
+
+        node = head3
+        debug = head3.next
+
+        while curr1 != None or curr2 != None:
+            if curr1 == None and curr2 != None:
+                node.next = curr2
+                curr2 = curr2.next
+            elif curr2 == None and curr1 != None:
+                node.next = curr1
+                curr1 = curr1.next
+            elif curr1.val >= curr2.val:
+                node.next = curr2
+                curr2 = curr2.next
+            else:
+                node.next = curr1
+                curr1 = curr1.next
+            node = node.next
+        return head3
+
+                
