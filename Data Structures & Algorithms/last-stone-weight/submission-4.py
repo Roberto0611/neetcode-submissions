c1632fba151be17a3251676857b3405d51a5b48a
Roberto Ochoa Cuevas <129466817+Roberto0611@users.noreply.github.com>
2026-08-31 11:08:48 -0600

    Add: last-stone-weight - submission-4

---
+++ Data Structures & Algorithms/last-stone-weight/submission-4.py
@@ -0,0 +1,38 @@
+import heapq
+
+"""
+Para esta solucion usamos un maxheap con el truco de usar la lista negativa,
+removemos las dos piedras y sacamos el valor de la nueva y lo agregamos de nuevo a la lista (O no agregamos nada si se destruye)
+
+complejidad: O(N log N)
+"""
+
+class Solution:
+    def lastStoneWeight(self, stones: List[int]) -> int:
+        # we make a max heap
+        # first we convert all to negative
+        for i in range(len(stones)):
+            stones[i] = -stones[i]
+        
+        # make the heap
+        heapq.heapify(stones)
+
+        # main loop
+        while len(stones) >= 2:
+            # we take the 2 max stones
+            x = -heapq.heappop(stones)
+            y = -heapq.heappop(stones)
+
+            # we smash the stones
+            if x == y:
+                continue
+            
+            y = y - x
+
+            # we add the new stone
+            heapq.heappush(stones,y)
+        
+        if len(stones) == 0:
+            return 0
+
+        return -stones[0]
