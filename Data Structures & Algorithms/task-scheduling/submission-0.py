4fd9ab3e1d3d5d97c4f2a0e3fb2241114bc87ab4
Roberto Ochoa Cuevas <129466817+Roberto0611@users.noreply.github.com>
2026-09-23 07:00:21 -0600

    Add: task-scheduling - submission-0

---
+++ Data Structures & Algorithms/task-scheduling/submission-0.py
@@ -0,0 +1,45 @@
+''' For this approach we are going to use a max heap with the number of iterations of a task, then we are going to pop the maximum value and add it to a queue in the format [iterations - 1,time_to_leave] when the queue is empty we return time'''
+
+class Solution:
+    def leastInterval(self, tasks: List[str], n: int) -> int:
+        time = 0
+        # counting the elements
+        count = {}
+        
+        for task in tasks:
+            count[task] = count.get(task,0) + 1;
+        
+        # max heap
+        heap = []
+        heapq.heapify(heap)
+
+        for task in count:
+            heapq.heappush(heap,-count[task]) # negative for max heap
+
+        # make queue
+        queue = []
+
+        # main loop
+        while True:
+            time += 1
+
+            if heap:
+                task = -heapq.heappop(heap) - 1
+
+                if task > 0:
+                    queue.append([task,time + n])
+
+            # check queue
+            if queue:
+                if queue[0][1] == time:
+                    task = queue.pop(0)
+                    heapq.heappush(heap,-task[0])
+            
+            if not queue and not heap:
+                break
+
+        return time
+
+
+
+        
